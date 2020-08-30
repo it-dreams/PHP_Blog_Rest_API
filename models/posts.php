58c90f0a0fbcbed83c7d3fb84c6a828d4a15b89b
it-dreams <rahulraj.holi888@gmail.com>
2020-08-30 12:41:23 +0530

    create model file for posts

---
+++ models/posts.php
@@ -0,0 +1,48 @@
+<?php 
+	Class Post {
+		// DB stuff
+		private $conn;
+		private $table ='posts';
+
+		// Post properties
+		public $id;
+		public $category_id;
+		public $category_name;
+		public $title;
+		public $body;
+		public $author;
+		public $created_at;
+
+		// Constructor with BD
+		public function __construct($db) {
+			$this->conn = $db;
+		}
+
+		// Get All Posts
+		public function read() {
+			// Created query
+			$query = 'SELECT 
+					c.name as category_name,
+					p.id,
+					p.category_id,
+					p.title,
+					p.body,
+					p.author,
+					p.created_at
+				FROM
+					' . $this->table . ' p
+				LEFT JOIN
+					categories c ON p.category_id = c.id
+				ORDER BY
+					p.created_at DESC';
+
+			// Prepare Statement
+			$stmt = $this->conn->prepare($query);
+
+			// Execute query
+			$stmt->execute();
+
+			return $stmt;
+		}
+	}
+?>
